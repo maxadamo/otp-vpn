1d839dd17fed77851d460aa1042fca0cf25c5540
Massimiliano Adamo <maxadamo@gmail.com>
2019-09-29 19:41:20 +0200

    fix guake

--- install.sh
+++ install.sh
@@ -158,11 +158,11 @@ cat <<EOF > ~/bin/jump_on.sh
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 if [ -f /usr/bin/guake ]; then
     python3 guake --rename-current-tab="Jump VPN" -e "sudo openvpn --config /home/maxadamo/.client.ovpn"
-    guake --show -s "Jump VPN"
+    guake --show
     sleep 4
     guake --hide
 else
-    rxvt -depth 32 -bg rgba:0000/0000/0000/9999 -fg "[99]green" --geometry 160x15 -title "Jump VPN" -e /bin/bash 
+    rxvt -depth 32 -bg rgba:0000/0000/0000/9999 -fg "[99]green" --geometry 160x15 -title "Jump VPN" -e /bin/bash \
         -c "sudo openvpn --config /home/maxadamo/.client.ovpn"
 fi
 EOF
